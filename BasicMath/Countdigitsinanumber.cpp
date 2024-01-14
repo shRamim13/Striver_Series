36c250d4b36d482e3a4919c55da13b448cdf3f27
shRamim13 <shramim13@gmail.com>
2024-01-15 00:14:26 +0600

    first commit

---
+++ BasicMath/Countdigitsinanumber.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int countingDegit(int n)
+{
+    int cnt = 0;
+    while (n>0)
+    {
+        n=n/10;
+        cnt++;
+    }
+    
+    return cnt;
+}
+int main()
+{
+   while (1)
+   {
+     int n;
+    cin >> n;
+    cout << countingDegit(n)<<endl;
+    cout<<int(log10(n)+1)<<endl;
+    /* code */
+   }
+   
+}
