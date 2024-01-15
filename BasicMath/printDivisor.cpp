5da9742ae12f09c0c90cc8459c06921fc1c33806
shRamim13 <shramim13@gmail.com>
2024-01-15 10:52:43 +0600

    divisor

---
+++ BasicMath/printDivisor.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> div;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n & i == 0)
+        {
+            div.push_back(i);
+            if (n % i != i)
+                div.push_back(n / i);
+        }
+    }
+    sort(div.begin())
+}
