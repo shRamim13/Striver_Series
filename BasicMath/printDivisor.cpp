4a94b46048d5e1e0225d4e7b4549d3f3a9132aa4
shRamim13 <shramim13@gmail.com>
2024-01-15 10:54:43 +0600

    1

--- BasicMath/printDivisor.cpp
+++ BasicMath/printDivisor.cpp
@@ -7,12 +7,13 @@ int main()
         int n;
         cin >> n;
         vector<int> div;
+
         for (int i = 1; i * i <= n; i++)
         {
             if (n & i == 0)
             {
                 div.push_back(i);
-                if (n % i != i)
+                if (n / i != i)
                     div.push_back(n / i);
             }
         }
