bf5e37308ad58f97d6535ea28e31a49a0aa8f2af
shRamim13 <shramim13@gmail.com>
2024-01-17 12:06:04 +0600

    bubbleSort

---
+++ sorting/BubbleSORT.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n)
+{
+    // bubble sort
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int swapCnt = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapCnt = 1;
+            }
+        }
+        if (swapCnt == 0)
+            break;
+    }
+
+    cout << "After Using bubble sort: "
+         << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using Bubble Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubble_sort(arr, n);
+    return 0;
+}
