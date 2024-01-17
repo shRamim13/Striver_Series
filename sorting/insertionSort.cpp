fd1f811b612b6e06fa5c24847b24c5b6d7200e7b
shRamim13 <shramim13@gmail.com>
2024-01-17 12:44:30 +0600

    insertion final

--- sorting/insertionSort.cpp
+++ sorting/insertionSort.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 void insertion_sort(int arr[], int n)
 {
-    for (int i = 0; i <= n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
         int j = i;
         while (j > 0 && arr[j - 1] > arr[j])
         {
-            int temp = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = temp;
+            swap(arr[j], arr[j - 1]);
             j--;
         }
     }
