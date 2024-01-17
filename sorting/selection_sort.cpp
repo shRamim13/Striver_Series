1398c5074dab6ebf7e6ac655d6c8aaf8b8851b47
shRamim13 <shramim13@gmail.com>
2024-01-17 11:41:06 +0600

    selectionSort

---
+++ sorting/selection_sort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void selection_sort(int arr[], int n)
+{
+    // selection sort
+    for (int i = 0; i < n - 2; i++)
+    {
+        int min_indx = i;
+        for (int j = i = 0; j < n - 1; j++)
+        {
+            if (arr[j] < arr[min_indx])
+                min_indx = j;
+        }
+        swap(arr[i], arr[min_indx]);
+    }
+
+    cout << "After selection sort: "
+         << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+int main()
+{
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before selection sort: "
+         << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+    selection_sort(arr, n);
+    return 0;
+}
